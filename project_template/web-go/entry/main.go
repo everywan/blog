fa656788e4e63925bcf9dc66f76173a55fc99422
wzs <zhensheng.five@gmail.com>
2020-03-21 20:41:17 +0800

    删除项目示例, 迁移到 github.com/xgxw/toddler-go

--- project_template/web-go/entry/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/xxx/xxx/internal/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- project_template/web-go/internal/codes/code.go
+++
@@ -1,8 +0,0 @@
-package codes
-
-const (
-	// NotFound 未找到
-	NotFound = "not_found"
-	// BadRequest 参数错误, 请求无法理解
-	BadRequest = "bad_request"
-)
